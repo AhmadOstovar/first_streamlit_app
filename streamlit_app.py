57042658a1680d5cb862bd15efb7d40ead395d67
AhmadOstovar <106535377+AhmadOstovar@users.noreply.github.com>
2023-02-28 09:52:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,4 @@ streamlit.title('My First Streamlit!')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🥑🍞
-Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥑🍞 Hard-Boiled Free-Range Egg')
